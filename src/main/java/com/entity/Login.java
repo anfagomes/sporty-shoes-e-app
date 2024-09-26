3be0a2edf0449d36526ed1cd890485fab3dee860
Andre Gomes <anfagomes@outlook.com>
2024-09-26 06:08:21 +0000

    Init entity package

---
+++ src/main/java/com/entity/Login.java
@@ -0,0 +1,62 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Login {
+	
+	@Id
+	private String emailId;
+	private String username;
+	private String password;
+	private Role role;
+	
+	
+	public Login(String emailId, String username, String password, Role role) {
+		super();
+		this.emailId = emailId;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+	
+	
+	public Login() {
+		super();
+	}
+
+
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	@Override
+	public String toString() {
+		return "Login [emailId=" + emailId + ", username=" + username + ", password=" + password + ", role=" + role
+				+ "]";
+	}
+	
+	
+
+}
